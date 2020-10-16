883bf99536264c7c69a88cedc4304aa14fe40be8
ehmicky <ehmicky@gmail.com>
2020-10-16 21:50:38 +0200

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  printWidth: 120,
+  proseWrap: 'always',
+  trailingComma: 'all',
+}
---
+++ test/fixtures/_valid.js
@@ -0,0 +1,3 @@
+const test = true
+
+module.exports = { test }
