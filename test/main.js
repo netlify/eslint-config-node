7657ff13af7b77ac60392dbe75add5bf0eac91c9
ehmicky <ehmicky@users.noreply.github.com>
2021-05-19 18:25:08 +0200

    chore: remove dead code (#197)

--- test/main.js
+++ test/main.js
@@ -7,8 +7,6 @@ const TEST_FILE = `${__dirname}/fixtures/_valid.js`
 test('Smoke test', async (t) => {
   const { exitCode, stdout, stderr } = await execa('eslint', [TEST_FILE, '--config', ESLINT_CONFIG], {
     preferLocal: true,
-    // @todo remove once https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore/pull/315 is merged and released
-    env: { NODE_OPTIONS: '--no-deprecation' },
   })
   t.is(exitCode, 0)
   t.is(stdout.trim(), '')
