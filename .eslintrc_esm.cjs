c245636194842b79c7293e3d4768dcda899ef893
ehmicky <ehmicky@users.noreply.github.com>
2022-02-02 20:51:24 +0100

    fix: missing file extension with ESM configuration (#443)

--- .eslintrc_esm.cjs
+++ .eslintrc_esm.cjs
@@ -1,4 +1,6 @@
-const baseEslintrc = require('./.eslintrc')
+// CommonJS allows omitting `.js` but not `.cjs`
+// eslint-disable-next-line import/extensions
+const baseEslintrc = require('./.eslintrc.cjs')
 
 // ESLint configuration for packages using pure ES modules.
 // This should be merged to the main `.eslintrc.js` once all our repositories
